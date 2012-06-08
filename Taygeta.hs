254b9bd1e4efc85af5e701ef61bfdf040081cb97
Eric Rochester <err8n@virginia.edu>
2012-06-08 15:21:20 -0400

    Added a script to display the twenty most frequent tokens in a text.

---
+++ Taygeta.hs
@@ -0,0 +1,47 @@
+
+-- Some playing with things as I read through *Foundations of Statistical
+-- Natural Language Processing* by Christopher Manning and Hinrich Shütze.
+
+module Main where
+
+import           Control.Applicative
+import           Control.Monad
+import           Data.Char
+import qualified Data.List as L
+import qualified Data.Map as M
+import           Data.Ord (comparing)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+-- import qualified Filesystem.Path.CurrentOS as FS
+-- import           Shelly
+import           System.Environment
+import           Text.Printf
+
+type Token   = T.Text
+type FreqMap = M.Map Token Int
+
+tokenize :: FilePath -> IO [Token]
+tokenize file =   filter (not . T.null)
+              .   map (T.filter isAlphaNum . T.toLower)
+              .   T.words
+              <$> TIO.readFile file
+
+countFreqs :: [Token] -> FreqMap
+countFreqs = L.foldl' inc M.empty
+    where inc m t = M.insertWith' (+) t 1 m
+
+sortFreqs :: FreqMap -> [(Token, Int)]
+sortFreqs = L.reverse . L.sortBy (comparing snd) . M.toList
+
+reportFreqs :: [(Token, Int)] -> String
+reportFreqs = L.foldl' format [] . L.reverse
+    where format s (t, f) = (printf "%20s %d\n" (T.unpack t) f) ++ s
+
+main :: IO ()
+main =
+    getArgs >>=
+    mapM tokenize >>=
+    putStrLn . reportFreqs . take 20 . sortFreqs . countFreqs . concat
+
+
+

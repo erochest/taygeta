6c51e5b91a8963fba5b6b0e5d4993eef8d348658
Eric Rochester <err8n@virginia.edu>
2012-06-08 23:39:34 -0400

    Added an inverted index, KWIC view, and refactored.

--- Taygeta.hs
+++ Taygeta.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 -- Some playing with things as I read through *Foundations of Statistical
 -- Natural Language Processing* by Christopher Manning and Hinrich Shütze.
@@ -9,7 +10,8 @@ import           Control.Monad
 import           Data.Char
 import qualified Data.List as L
 import qualified Data.Map as M
-import           Data.Maybe (mapMaybe)
+import           Data.Maybe (maybe, mapMaybe)
+import           Data.Monoid
 import           Data.Ord (comparing)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -17,11 +19,13 @@ import qualified Data.Text.IO as TIO
 -- import           Shelly
 import           System.Environment
 import           Text.Printf
+import           Debug.Trace
 
-type Token       = T.Text
-type Location    = Int
-type Bigram      = (Token, Token)
-type FreqMap a   = M.Map a Int
+type Token         = T.Text
+type Location      = Int
+type Bigram        = (Token, Token)
+type FreqMap a     = M.Map a Int
+type InvertedIndex = M.Map Token [Location]
 
 tokenizeFile :: FilePath -> IO [(Location, Token)]
 tokenizeFile = fmap tokenize . TIO.readFile
@@ -61,49 +65,96 @@ reportFreqs = L.foldl' format [] . L.reverse . zip [1..]
         format s (r, (a, f)) =
             (printf "%4d. %20s %-5d %d\n" r (show a) f (r * f)) ++ s
 
+indexTokens :: [(Location, Token)] -> InvertedIndex
+indexTokens = L.foldl' alter M.empty
+    where
+        alter m (l, t) = M.alter (alter' l) t m
+
+        alter' l (Just ls) = Just (l:ls)
+        alter' l Nothing   = Just [l]
+
+kwic :: T.Text -> Int -> InvertedIndex -> T.Text -> [T.Text]
+kwic text width index target =
+    map getContext . L.sort $ M.findWithDefault [] target index
+    where
+        csize :: Int
+        csize   = round $ (fromIntegral width / 2) - (fromIntegral $ T.length target) / 2
+
+        rmnl '\n' = ' '
+        rmnl '\r' = ' '
+        rmnl c    = c
+
+        target' = T.toLower target'
+        text'   = T.map rmnl text
+
+        -- Umm. There has to be a better way.
+        getContext l =
+            let start    = max 0 $ l - csize
+                c        = T.take width $ T.drop start text'
+                (p, c')  = T.breakOn " " c
+                (c'', _) = T.breakOnEnd " " c'
+                context  = (T.pack . take (T.length p) $ repeat ' ') `mappend` c''
+            in  if start > 0
+                    then context
+                    else (T.pack . take (abs $ l - csize) $ repeat ' ') `mappend` context
+
+
+nl :: IO ()
+nl = putStrLn ""
+
+header :: String -> IO ()
+header filename = do
+    putStrLn filename
+    putStrLn . take (length filename) $ repeat '='
+
+tokenFreqReport :: FreqMap Token -> IO ()
+tokenFreqReport =
+    -- Top 20 most frequent tokens
+    putStrLn . reportFreqs . take 20 . sortFreqs
+
+freqReport :: [Token] -> FreqMap Token -> IO ()
+freqReport tokens freqs = do
+    -- General frequency statistics
+    printf "Total tokens = %d\n" (length tokens)
+    printf "Total types  = %d\n" (M.size freqs)
+
+freqFreqReport :: FreqMap Int -> IO ()
+freqFreqReport = 
+    -- Frequency of frequencies
+    putStrLn . reportFreqs . L.sortBy (comparing fst) . M.toList
+
+bigramReport :: FreqMap Bigram -> IO ()
+bigramReport =
+    -- Frequency of bigrams
+    putStrLn . reportFreqs . take 20 . sortFreqs
+
+
 main :: IO ()
 main = do
     args <- getArgs
-    forM_ args $ \filename -> do
-        text <- TIO.readFile filename
-        let tokens  = tokenize text
-            tokens' = map snd tokens
+    (filename, mtarget) <- case args of
+        (f:t:_) -> return (f, Just t)
+        (f:_)   -> return (f, Nothing)
+        []      -> fail "You must provide a filename to process."
 
-            freqs   = countFreqs tokens'
-            ffreqs  = countFreqs $ M.elems freqs
+    text <- TIO.readFile filename
+    let tokens  = tokenize text
+        tokens' = map snd tokens
 
-            bigs    = bigrams tokens'
-            bfreqs  = countFreqs bigs
+        freqs   = countFreqs tokens'
+        ffreqs  = countFreqs $ M.elems freqs
 
-        header filename             >> nl
-        tokenFreqReport freqs       >> nl
-        freqReport tokens' freqs    >> nl
-        freqFreqReport ffreqs       >> nl
-        bigramReport bfreqs         >> nl
+        bigs    = bigrams tokens'
+        bfreqs  = countFreqs bigs
 
-    where
-        nl = putStrLn ""
+        index   = indexTokens tokens
 
-        header filename = do
-            putStrLn filename
-            putStrLn . take (length filename) $ repeat '='
+    header filename             >> nl
+    tokenFreqReport freqs       >> nl
+    freqReport tokens' freqs    >> nl
+    freqFreqReport ffreqs       >> nl
+    bigramReport bfreqs         >> nl
 
-        tokenFreqReport =
-            -- Top 20 most frequent tokens
-            putStrLn . reportFreqs . take 20 . sortFreqs
-
-        freqReport :: [Token] -> FreqMap Token -> IO ()
-        freqReport tokens freqs = do
-            -- General frequency statistics
-            printf "Total tokens = %d\n" (length tokens)
-            printf "Total types  = %d\n" (M.size freqs)
-
-        freqFreqReport = 
-            -- Frequency of frequencies
-            putStrLn . reportFreqs . L.sortBy (comparing fst) . M.toList
-
-        bigramReport =
-            -- Frequency of bigrams
-            putStrLn . reportFreqs . take 20 . sortFreqs
+    mapM_ (putStrLn . T.unpack) $ maybe [] (kwic text 78 index . T.pack) mtarget
 
 

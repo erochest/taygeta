97ef159d75042b5bb7c57932bcad819df03ff4fd
Eric Rochester <err8n@virginia.edu>
2012-08-02 14:53:45 -0400

    Fill in specs for tokenizer.

--- Test/TestTaygeta.hs
+++ Test/TestTaygeta.hs
@@ -1,35 +1,87 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Main where
 
-import Text.Taygeta.Tokenizer
-import Test.Hspec
+import           Data.Char
+import qualified Data.List as L
+import qualified Data.Text as T
+import qualified Filesystem.Path.CurrentOS as P
+import           Text.Taygeta.Tokenizer
+import           Test.Hspec
+import           Test.QuickCheck
+
+instance Arbitrary T.Text where
+    arbitrary = T.pack `fmap` arbitrary
+    shrink    = shrinkNothing
+
+-- Properties
+
+pLengthSum :: T.Text -> Bool
+pLengthSum input =
+    T.length input == (sum $ map (T.length . tokenRaw . snd) (tokenize P.empty 0 input))
+
+pIdem :: T.Text -> Bool
+pIdem input =
+    input == (T.concat $ map (tokenRaw . snd) (tokenize P.empty 0 input))
+
+-- Generators
+
+genList :: String -> Gen T.Text
+genList = fmap (T.intercalate " ") . listOf1 . genTextChars
+
+genTextChars :: String -> Gen T.Text
+genTextChars set =
+    fmap T.pack . listOf1 $ elements set
+
+genAlphaList :: Gen T.Text
+genAlphaList = genList $ ['a'..'z'] ++ ['A'..'Z']
+
+genNumberList :: Gen T.Text
+genNumberList = genList $ ['0'..'9']
+
+genPunctuationList :: Gen T.Text
+genPunctuationList = genTextChars $ filter isPunctuation [minBound..maxBound]
+
+genPrintList :: Gen T.Text
+genPrintList = genTextChars $ filter isPrint [minBound..maxBound]
+
+genSpaceList :: Gen T.Text
+genSpaceList = genTextChars $ filter isSpace [minBound..maxBound]
+
+-- Predicates
+
+allSame :: Eq a => [a] -> Bool
+allSame []     = False
+allSame (x:xs) = L.all (== x) xs
+
+allTokensAre :: Gen T.Text -> (Char -> Bool) -> Property
+allTokensAre gen p = forAll gen $ \n ->
+    let tokens = filter ((" " /=) . tokenRaw) . map snd $ tokenize P.empty 0 n
+    in  (length tokens > 0) .&&. (L.all (T.all p) $ map tokenRaw tokens)
+
+-- Specifications
 
 main :: IO ()
 main = hspec $ do
     describe "The tokenizer" $ do
-        it "should return a list of tokens" $
-            pending "not implemented"
-
         it "should return a list of tokens, the sum of whose lengths is the length of the input" $
-            pending "not implemented"
+            property pLengthSum
 
         it "should be able to recreate the input from the raw output" $
-            pending "not implemented"
+            property pIdem
 
-        it "should tokenize alphabetic tokens" $
-            pending "not implemented"
+        it "should tokenize alphabetic tokens" $ property $
+            allTokensAre genAlphaList isAlpha
 
-        it "should tokenize numeric tokens" $
-            pending "not implemented"
+        it "should tokenize numeric tokens" $ property $
+            allTokensAre genNumberList isDigit
 
         it "should tokenize punctuation" $
-            pending "not implemented"
-
-        it "should tokenize random characters" $
-            pending "not implemented"
+            allTokensAre genPunctuationList isPunctuation
 
         it "should return whitespace" $
-            pending "not implemented"
+            allTokensAre genSpaceList isSpace
 
     describe "The number filter" $ do
         it "should join numbers with common number punctuation" $

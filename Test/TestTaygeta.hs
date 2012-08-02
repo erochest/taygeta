1705c9e6b027d7da4b206abc1a291b7ea67cea68
Eric Rochester <err8n@virginia.edu>
2012-08-02 11:59:00 -0400

    Stub out specs for tokenizer.

--- Test/TestTaygeta.hs
+++ Test/TestTaygeta.hs
@@ -1,13 +1,44 @@
 
 module Main where
 
+import Text.Taygeta.Tokenizer
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-    describe "the test framework" $ do
-        it "should fail sometimes" $
-            (1 :: Int) `shouldBe` (2 :: Int)
-        it "should pass sometimes" $
-            (1 :: Int) `shouldBe` (1 :: Int)
+    describe "The tokenizer" $ do
+        it "should return a list of tokens" $
+            pending "not implemented"
+
+        it "should return a list of tokens, the sum of whose lengths is the length of the input" $
+            pending "not implemented"
+
+        it "should be able to recreate the input from the raw output" $
+            pending "not implemented"
+
+        it "should tokenize alphabetic tokens" $
+            pending "not implemented"
+
+        it "should tokenize numeric tokens" $
+            pending "not implemented"
+
+        it "should tokenize punctuation" $
+            pending "not implemented"
+
+        it "should tokenize random characters" $
+            pending "not implemented"
+
+        it "should return whitespace" $
+            pending "not implemented"
+
+    describe "The number filter" $ do
+        it "should join numbers with common number punctuation" $
+            pending "not implemented"
+
+    describe "The English token filter" $ do
+        it "should remove whitespace" $
+            pending "not implemented"
+
+        it "join contractions" $
+            pending "not implemented"
 
---
+++ Text/Taygeta/Tokenizer.hs
@@ -0,0 +1,5 @@
+
+module Text.Taygeta.Tokenizer
+    (
+    ) where
+

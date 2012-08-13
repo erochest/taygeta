96a9d4a4fb06ab14c6f60205386f36b5bdfd0809
Eric Rochester <err8n@virginia.edu>
2012-08-13 09:06:11 -0400

    Add conduit to chunk text input on a text string.

--- Taygeta/Corpus.hs
+++ Taygeta/Corpus.hs
@@ -19,6 +19,7 @@ module Taygeta.Corpus
     , moveOffset'
     , setOffset
     , moveOffset
+    , chunkOn
     ) where
 
 
@@ -153,4 +154,22 @@ instance (Applicative m, MonadIO m, C.MonadThrow m, C.MonadUnsafeIO m)
             Just fp -> do
                 sourceFile fp
                 getDocuments
+
+-- This chunks on a text, returning the prefix each time, and the next time
+-- concatenating the rest with the prefix from the next input.
+chunkOn :: Monad m => T.Text -> C.Conduit T.Text m T.Text
+chunkOn t = loop T.empty
+    where
+        loop prefix = do
+            text' <- C.await
+            case text' of
+                Nothing -> handleTail prefix
+                Just text -> do
+                    let (current, next) = T.breakOnEnd t text
+                    C.yield current
+                    loop next
+
+        handleTail prefix | T.null prefix = return ()
+                          | otherwise     = C.leftover prefix >> return ()
+
 

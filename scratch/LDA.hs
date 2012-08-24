174b0edde2a13d3279461361f9670fff9bf0df56
Eric Rochester <err8n@virginia.edu>
2012-08-24 14:00:42 -0400

    Stub out script to play with implementing LDA.

---
+++ scratch/LDA.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | This is an attempt to implement an LDA algorithm in Haskell. Initially,
+-- I'll be cribbing off of the [online lda
+-- package](http://www.cs.princeton.edu/~blei/downloads/onlineldavb.tar).
+
+module Main where
+
+
+main :: IO ()
+main = putStrLn "LDA"
+
+

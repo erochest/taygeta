bb5e32b1e486973e11b706ad9991f4862570462b
Eric Rochester <err8n@virginia.edu>
2012-08-31 12:39:30 -0400

    Data.Taygeta.Freqs.

---
+++ Data/Taygeta/Freqs.hs
@@ -0,0 +1,21 @@
+
+module Data.Taygeta.Freqs
+    ( FreqMap
+    , countFreqsC
+    , countFreqs
+    ) where
+
+import qualified Data.Conduit as C
+import qualified Data.Conduit.List as CL
+import           Data.Hashable
+import qualified Data.HashMap.Strict as M
+
+type FreqMap a     = M.HashMap a Int
+
+countFreqs :: (Ord a, Hashable a) => [a] -> FreqMap a
+countFreqs = M.fromListWith (+) . flip zip (repeat 1)
+
+countFreqsC :: (Monad m, Ord a, Hashable a) => C.Sink a m (FreqMap a)
+countFreqsC = CL.fold inc M.empty
+    where inc m t = M.insertWith (+) t 1 m
+
--- Test/TestTaygeta.hs
+++ Test/TestTaygeta.hs
@@ -8,8 +8,10 @@ import           Control.Monad.Trans.Resource
 import           Data.Char
 import qualified Data.Conduit as C
 import qualified Data.Conduit.List as CL
-import           Data.Monoid
+import qualified Data.HashMap.Strict as M
 import qualified Data.List as L
+import           Data.Monoid
+import           Data.Taygeta.Freqs
 import qualified Data.Text as T
 -- import qualified Filesystem.Path.CurrentOS as P
 import           Text.Taygeta.Tokenizer
@@ -66,11 +68,10 @@ main = hspec $ do
                 tokenz = Token tz $ normalize tz
             in  mappend tokenx (mappend tokeny tokenz) == mappend (mappend tokenx tokeny) tokenz
 
-    describe "The number filter" $ do
+    describe "The number filter" $
         it "should join numbers with common number punctuation" $ do
             let numbers = tokenC C.=$= numberFilter
-                shouldParseTo input expected =
-                    (input `shouldParseWith` numbers) expected
+                shouldParseTo input = input `shouldParseWith` numbers
             "1"            `shouldParseTo` ["1"]
             "12"           `shouldParseTo` ["12"]
             "199.99"       `shouldParseTo` ["199.99"]
@@ -79,18 +80,16 @@ main = hspec $ do
             "1,22. "       `shouldParseTo` ["1,22", ".", " "]
             "1,,23.,45."   `shouldParseTo` ["1", ",", ",", "23", ".", ",", "45", "."]
 
-    describe "The whitespace filter" $ do
+    describe "The whitespace filter" $
         it "should remove whitespace" $ do
             let ws = tokenC C.=$= whitespaceFilter
-                shouldParseTo input expected =
-                    (input `shouldParseWith` ws) expected
+                shouldParseTo input = input `shouldParseWith` ws
             "a b c" `shouldParseTo` ["a", "b", "c"]
 
-    describe "The English token filter" $ do
+    describe "The English token filter" $
         it "should join contractions" $ do
             let english = tokenC C.=$= englishFilter C.=$= whitespaceFilter
-                shouldParseTo input expected =
-                    (input `shouldParseWith` english) expected
+                shouldParseTo input = input `shouldParseWith` english
             "a, b, c"           `shouldParseTo` ["a", ",", "b", ",", "c"]
             "a, don't tell me!" `shouldParseTo` [ "a", ",", "don't", "tell", "me", "!" ]
 
@@ -99,22 +98,21 @@ main = hspec $ do
                     C.=$= numberFilter
                     C.=$= englishFilter
                     C.=$= alphaNumericFilter
-            shouldParseTo input expected =
-                (input `shouldParseWith` conduit) expected
+            shouldParseTo input = input `shouldParseWith` conduit
 
-        it "should remove whitespace" $ do
+        it "should remove whitespace" $
             "a b c" `shouldParseTo` ["a", "b", "c"]
 
-        it "should remove punctuation" $ do
+        it "should remove punctuation" $
             "a-b,c!" `shouldParseTo` ["a", "b", "c"]
 
-        it "should leave words" $ do
+        it "should leave words" $
             "these are some words!" `shouldParseTo` ["these", "are", "some", "words"]
 
-        it "should leave formatted numbers" $ do
+        it "should leave formatted numbers" $
             "a number: 1,000.00" `shouldParseTo` ["a", "number", "1,000.00"]
 
-        it "should leave contractions" $ do
+        it "should leave contractions" $
             "doesn't disappear, y'all" `shouldParseTo` ["doesn't", "disappear", "y'all"]
 
     describe "The English stop-list filter" $ do
@@ -123,12 +121,22 @@ main = hspec $ do
                     C.=$= englishFilter
                     C.=$= alphaNumericFilter
                     C.=$= englishStopListFilter
-            shouldParseTo input expected =
-                (input `shouldParseWith` conduit) expected
+            shouldParseTo input =
+                (input `shouldParseWith` conduit)
 
         it "should remove common English words" $ do
             "This, that, and the other" `shouldParseTo` []
             "The Cat in the Hat"        `shouldParseTo` ["cat", "hat"]
+
+    describe "countFreqs" $ do
+        it "should assign all unique items a frequency of one" $
+            L.all (==1) . M.elems $ countFreqs ([1, 2, 3, 4] :: [Int])
+
+        it "should count the number of each item type in the input" $
+            let expected = [('a', 1), ('b', 2), ('c', 3), ('d', 4)]
+                input    = "dbacddcbcd"
+            in  L.and . L.zipWith (==) expected . L.sort . M.toList
+                      $ countFreqs input
 
 instance Arbitrary T.Text where
     arbitrary = T.pack `fmap` arbitrary
@@ -137,7 +145,7 @@ instance Arbitrary T.Text where
 -- Utilities
 
 edefault :: c -> (b -> c) -> Either a b -> c
-edefault d f e = either (const d) f e
+edefault d = either (const d)
 
 tokenize' :: T.Text -> [Token]
 tokenize' input = runTokenConduit input tokenC
@@ -161,17 +169,17 @@ shouldParseWith :: T.Text
                 -> C.Conduit T.Text (ExceptionT Identity) TokenPos
                 -> [T.Text] -> Expectation
 shouldParseWith input filterC expected =
-   (map tokenText (runTokenConduit input filterC)) `shouldBe` expected
+   map tokenText (runTokenConduit input filterC) `shouldBe` expected
 
 -- Properties
 
 pLengthSum :: T.Text -> Bool
 pLengthSum input =
-    T.length input == (sum $ map (T.length . tokenRaw) (tokenize' input))
+    (T.length input ==) . sum . map (T.length . tokenRaw) $ tokenize' input
 
 pIdem :: T.Text -> Bool
 pIdem input =
-    input == (T.concat $ map tokenRaw (tokenize' input))
+    (input ==) . T.concat . map tokenRaw $ tokenize' input
 
 -- Generators
 
@@ -186,7 +194,7 @@ genAlphaList :: Gen T.Text
 genAlphaList = genList $ ['a'..'z'] ++ ['A'..'Z']
 
 genNumberList :: Gen T.Text
-genNumberList = genList $ ['0'..'9']
+genNumberList = genList ['0'..'9']
 
 genPunctuationList :: Gen T.Text
 genPunctuationList = genTextChars $ getChars isPunctuation
